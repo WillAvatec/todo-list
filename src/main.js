c1b61bbad8d6d7480f64f467c3cfcef072258e97
Will <avatec2312@gmail.com>
2022-12-09 03:22:52 -0500

    Using getters and setters for private property

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 class Todo {
+    #title;
+    
     constructor(title){
-        this._title = title;
+        this.setTitle(title);
         this._description = "gibberish";
         this._dueDate = "2022-12-09";
         this._priority = "low";
     }
-
+    getTitle(){
+        return this.#title;
+    }
+    setTitle(value){
+        this.#title = value;
+    }    
 }
+
+const one = new Todo("lIfe");
