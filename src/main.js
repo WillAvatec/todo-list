366d794571b9afe459a61d965bfdb64c452c78e1
Will <avatec2312@gmail.com>
2022-12-09 03:03:04 -0500

    Create todo class initialize only with Constructor

---
+++ src/main.js
@@ -0,0 +1,9 @@
+class Todo {
+    constructor(title){
+        this._title = title;
+        this._description = "gibberish";
+        this._dueDate = "2022-12-09";
+        this._priority = "low";
+    }
+
+}
