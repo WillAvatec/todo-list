5d53fc9c0a1ac9712cbc02c7520d523d6785da43
Will <avatec2312@gmail.com>
2022-12-10 13:09:38 -0500

    Logic for change the Todos

---
+++ src/modules/todo_logic.js
@@ -0,0 +1,19 @@
+import Todo from './basic_todo.js';
+
+// The logic of the app, is going to be:
+
+    // Create projects arrays,
+    // Create a todo,(Just use the => [new Todo] <=<)
+    // Add it to the current project(Added method into class)
+    // 
+
+
+
+const one = new Todo("memo");
+one.setPriority("high");
+one.pushArray();
+
+
+
+
+
