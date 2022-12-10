95c49e57e117d1aca71213ccebe3b85824d17544
Will <avatec2312@gmail.com>
2022-12-10 13:09:05 -0500

    Creating getter fuctions

--- src/modules/basic_todo.js
+++ src/modules/basic_todo.js
@@ -4,18 +4,30 @@
 
 export default class Todo {
         #title;
+        #priority;
         
-        constructor(title){
+        constructor(
+            title="",
+            priority)
+            {
             this.setTitle(title);
+            this.setPriority(priority);
             this._description = "gibberish";
-            this._dueDate = "2022-12-09";
-            this._priority = "low";
+            this._dueDate = "2023";
         }
         getTitle(){
             return this.#title;
         }
         setTitle(value){
             this.#title = value;
+        }
+        getPriority(){
+            return this.#priority;
+        }
+        setPriority(value){
+            if(value == "high" || value == "low") return this.#priority = value;
+            console.log("Invalid value");
+            this.#priority = 'low';
         }    
         pushArray(project){
             if(project.includes(this)) return console.log("Ya esta en el array.")
