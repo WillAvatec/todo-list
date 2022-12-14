f54fe215d9e1734adec16f22a940dca820cef3cf
Will <avatec2312@gmail.com>
2022-12-14 05:03:57 -0500

    Using simple template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import { formatDistance, subDays } from "date-fns";
-import { elementHelper } from "./modules/Classes/elementCreator";
+import Project from '../src/modules/Classes/projects.js';
+import { renderTodo } from "./modules/renderTodo.js";
+import { form_handler } from "./modules/Classes/DOMDataHandle.js";
+import Todo from "./modules/Classes/todo";
 
-const one = formatDistance(subDays(new Date(),5),new Date(),{addSuffix:true});
+// When user clicks new project create current project
 
-const container = document.querySelector('.container');
+const defaultProject = new Project('default');
 
+const one = new Todo('Love','low','2022','not yet')
+
+const newProjectBtn = document.querySelector('.add-project-btn');
+
+newProjectBtn.addEventListener('click',()=>{
+    renderTodo(one)
+})
+
+
+
+// When the user clicks change DOM main into a form
+
+/* const newTodoBtn = document.querySelector('todoBtn');
+
+newTodoBtn.addEventListener('click',()=>{
+    
+
+    // Replace main content with a form for the 
+    // with a button that returns an Todo object
+}) */
+
+// When the user clicks on the Create button, 
