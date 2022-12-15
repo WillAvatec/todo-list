790cb45727799d5b11acda191bc40a7016034928
Will <avatec2312@gmail.com>
2022-12-15 10:30:46 -0500

    adding conditional statement

--- src/modules/Classes/DOMDataHandle.js
+++ src/modules/Classes/DOMDataHandle.js
@@ -7,17 +7,22 @@ export const form_handler = () => {
 
     const dataTitle = document.querySelector('#title').value;
     const dataPrior = document.querySelector('input[name="priority"]:checked').value;
+
+    if(dataPrior == null) return alert('Must select a priority');
+
     let el = new Todo(dataTitle,dataPrior);
     return el
-
 }
+
+// This function will be in charge of creating a new 'Project',
+// Whenever the newProjectButton is clicked, also makes a
+// simple input on the form, so maybe this could need function inside
 
 export const folder_handler = () => {
 
-    // This function will be in charge of creating a new 'Project',
-    // Whenever the newProjectButton is clicked, also makes a
-    // simple input on the form, so maybe this could need function inside
-    
+    const name = document.querySelector()    
+
+
     function folderInput(){
 
         // This will create an input whenever the button is touched
